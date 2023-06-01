bc97ab601fde78c19bd1b801e8a8ac941c8adb9f
Khaled Nassar <7225227+notkmhn@users.noreply.github.com>
2023-06-01 15:39:20 +0200

    Update chntpw.rb

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -2,9 +2,9 @@ class Chntpw < Formula
 
     desc "The Offline NT Password Editor"
     homepage "https://github.com/minacle/chntpw"
-    url "https://github.com/minacle/chntpw/archive/refs/tags/chntpw/1.0.1.tar.gz"
+    url "https://github.com/notkmhn/chntpw/archive/refs/tags/chntpw/1.0.1.tar.gz"
     sha256 "474b99bc2bd524e9012d5d038497011028886fdc4582f70ad88546ff216483ee"
-    head "https://github.com/minacle/chntpw"
+    head "https://github.com/notkmhn/chntpw"
     license "GPL-2.0-only"
     depends_on "openssl@1.1" => :build
 
